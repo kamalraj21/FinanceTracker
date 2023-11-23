414426c7968ba71b472a6ddaf875aa8d051d540a
Kamal Raj <kamal.raj2181@gmail.com>
2023-11-23 18:54:45 +0530

    updated script for fixes

--- scripts/tag_extractor.py
+++ scripts/tag_extractor.py
@@ -6,61 +6,47 @@ import re
 import yaml
 from jinja2 import Environment, FileSystemLoader, select_autoescape
 
-class TagExtractor(Preprocessor):
+class TagExtractor(Extension):
+    def extendMarkdown(self, md):
+        md.registerExtension(self)
+        md.preprocessors.register(TagPreprocessor(md), 'tag_extractor', 27)
+
+class TagPreprocessor(Preprocessor):
     def run(self, lines):
         content = "\n".join(lines)
-        # Extract YAML front matter...
-        
-        # Extract tagged sections
-        tag_pattern = re.compile(r'\[Tag: (.+?)\]')
-        tagged_content = {}
-        for tag in tag_pattern.findall(content):
-            start = content.find(f"[Tag: {tag}]") + len(f"[Tag: {tag}]")
-            end = content.find("[Tag:", start)
-            tagged_content[tag] = content[start:end].strip() if end != -1 else content[start:].strip()
-
-        return yaml_content, tagged_content
-
-class TagExtractorExtension(Extension):
-    def extendMarkdown(self, md):
-        md.preprocessors.register(TagExtractor(md), 'tag_extractor', 27)
+        yaml_content = {}
+        front_matter = re.search(r'^---(.+?)---', content, re.DOTALL)
+        if front_matter:
+            yaml_content = yaml.safe_load(front_matter.group(1))
+            content = content[front_matter.end():]  # Adjust content to exclude front matter
+        content_without_tags = re.sub(r'\[Tag: .+?\]', '', content).strip()
+        return [content_without_tags], yaml_content
 
 def parse_markdown(file_path):
     with open(file_path, 'r', encoding='utf-8') as file:
-        text = file.readlines()
-    md = markdown.Markdown(extensions=[TagExtractorExtension()])
-    front_matter, tagged_content = md.preprocessors['tag_extractor'].run(text)
-    return front_matter, tagged_content
+        text = file.read()
+    md = markdown.Markdown(extensions=[TagExtractor()])
+    content, yaml_content = md.convert(text)
+    return yaml_content, content
 
 def parse_all_markdown_files(docs_path):
     all_content = {}
     for filename in os.listdir(docs_path):
         if filename.endswith(".md"):
             file_path = os.path.join(docs_path, filename)
-            front_matter, tagged_content = parse_markdown(file_path)
-            all_content[filename] = {'front_matter': front_matter, 'tagged_content': tagged_content}
+            yaml_content, content = parse_markdown(file_path)
+            all_content[filename] = {'front_matter': yaml_content, 'content': content}
             print(f"Processed {filename}")
     return all_content
 
 def render_to_html(template_name, content, output_filename):
-    # Ensure the output directory exists
-    output_dir = os.path.dirname(output_filename)
-    if not os.path.exists(output_dir):
-        os.makedirs(output_dir, exist_ok=True)
-    
-    # Load the template environment
     env = Environment(
         loader=FileSystemLoader(searchpath='../html_templates'),
         autoescape=select_autoescape(['html', 'xml'])
     )
-
-    # Select the template file
     template = env.get_template(template_name)
-
-    # Render the template with content
     html_content = template.render(content=content)
-
-    # Write the rendered HTML to file
+    os.makedirs(os.path.dirname(output_filename), exist_ok=True)
     with open(output_filename, 'w', encoding='utf-8') as file:
         file.write(html_content)
     print(f"{output_filename} has been created.")
@@ -68,12 +54,8 @@ def render_to_html(template_name, content, output_filename):
 def main():
     docs_path = '../docs'
     all_extracted_content = parse_all_markdown_files(docs_path)
-
-    # Render each Markdown file to an HTML file
     for filename, content in all_extracted_content.items():
-        # Define the output HTML filename based on the Markdown filename
         output_filename = f"../html_outputs/{filename.replace('.md', '.html')}"
-        # Assuming you have a generic template for all files
         render_to_html('generic_template.html', content, output_filename)
 
 if __name__ == "__main__":
